fec8041c7f1d72057bc03387cd87803ebeb81e86
Eddie Moon <eddiemoonie@gmail.com>
2018-02-26 23:01:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,4 +26,17 @@ class Song
     @@artists.uniq
   end
 
+  def self.genre_count
+    genre_count = {}
+    @@genre_count.each do |genre|
+      if genre_count[genre]
+        genre_count += 1
+      else
+        genre_count[genre] = 1
+      end
+  end
+
+  def self.artist_count
+  end
+
 end
