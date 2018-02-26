80ea43f5d99e67fd2e5de140086b1ebed2a9934f
Eddie Moon <eddiemoonie@gmail.com>
2018-02-26 22:37:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,13 +3,14 @@ class Song
   @@count = 0
   @@artists = 0
   @@genres = 0
-  
+
   def initialize(name, artist, genre)
     @@count += 1
   end
 
   def self.name
-    
+
+  end
 
   def self.count
     @@count
